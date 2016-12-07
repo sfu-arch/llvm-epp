008d9acf3c58b18526ba7900fee6bc2c9b4ef2b3
Snehasish Kumar <ska124@sfu.ca>
2016-12-07 22:15:04 +0530

    Added basic test source, testing works. Now all we need are actual tests

--- test/c/01-basic.c
+++
@@ -1,7 +0,0 @@
-
-int main(int argc, char* argv[]) { return 0; }
-
-// RUN: clang -c -g -emit-llvm %s -o %T/%t.bc 
-// RUN: llvm-epp %T/%t.bc -o %T/%t
-// RUN: clang %T/%t.epp.bc -o %T/test-exec
-// RUN: %T/text-exec 
---
+++ test/srcs/01-basic.c
@@ -0,0 +1,7 @@
+
+int main(int argc, char* argv[]) { return 0; }
+
+// RUN: clang -c -g -emit-llvm %s -o %t.bc 
+// RUN: llvm-epp -epp-fn=main %t.bc -o %t 2>&1 | tail -n 1 > %t1
+// RUN: echo "NumPaths : 1" > %t2
+// RUN: diff %t1 %t2 
